101ed62a15c576670c6f477e529dc11151ed404d
saitarun135 <saitarun135@gm.com>
2021-03-10 12:11:51 +0530

    initializing the variable

--- game.java
+++ game.java
@@ -1,5 +1,10 @@
 public class game{ 
 	public static void main(String args[]){
-	System.out.println("Welcome to the snake and ladders game");
+	 int pos=0;
+	 int temp_pos=0;
+	 int dieRoll=0;
+	 int flag=0;
+	 System.out.println("Welcome to the snake and ladder game");
+
 	}
 }
